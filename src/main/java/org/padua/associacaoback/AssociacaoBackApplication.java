3c0d8e17fb601556f1ce8fa6cd1bf40d5ed793b7
Rafael Pádua <rafaelpadua07@gmail.com>
2025-01-20 22:29:49 -0400

    first commit

---
+++ src/main/java/org/padua/associacaoback/AssociacaoBackApplication.java
@@ -0,0 +1,13 @@
+package org.padua.associacaoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssociacaoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssociacaoBackApplication.class, args);
+    }
+
+}
